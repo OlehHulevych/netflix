8340120238a04d920899edbf8b881c391f6f3c8a
Oleh <oleggulevic8@gmail.com>
2024-09-05 09:11:21 +0200

    add adding movie page and made it responsive

--- client/src/pages/AdminPanel/AddMoviePage.tsx
+++ client/src/pages/AdminPanel/AddMoviePage.tsx
@@ -1,37 +1,59 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const AddMoviePage = () => {
     return (
-        <main className={"w-full min-h-screen text-white font-inter"}>
-            <form action="">
-                <label htmlFor="">Name</label>
-                <input type="text"/>
-                <label htmlFor="">Year</label>
-                <input type="text"/>
-                <label htmlFor="">Duration</label>
-                <input type="text"/>
-                <label htmlFor="type">type</label>
-                <select name="type" id="type">
-                    <option value="Movie">Movie</option>
-                    <option value="Tv Shows">Tv shows</option>
-                </select>
-                <label htmlFor="type">Genre</label>
-                <select name="type" id="type">
-                    <option value="Action">Action</option>
-                    <option value="Comedy">Comedy</option>
-                    <option value="Fantasize">Fantasize</option>
-                </select>
-                <label htmlFor="">Banner image</label>
-                <input type="file" id={"Banner image"}/>
-
-                <label htmlFor="description">Name image</label>
-                <input type="file" id={"Name image"}/>
-
-                <label htmlFor="description">description</label>
-                <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        <>
+            <header className={"w-full h-4 bg-black-500  z-20 max-[768px]:mb-8"}>
+                <Link to={"/"}>
+                    <img src="/images/Netflix_Logo_1.png" alt="" className={"w-[12%] h-1/8 max-[900px]:w-3/12 cursor-pointer"}/>
+                </Link>
+            </header>
+        <main className={"w-full min-h-screen flex justify-center items-center overflow-hidden    text-white font-inter"}>
+            <form className={"w-full flex flex-col text-black align-middle max-[600px]:ml-12"} action="">
+                <div className={"mb-2 align-middle flex flex-col "}>
+                    <label  className={"text-white mr-2"}  htmlFor="">Name</label>
+                    <input className={"w-3/4 py-1 px-2 rounded-sm bg-slate-700 outline-none"} type="text"/>
+                </div>
+                <div className={"mb-2  flex flex-col "}>
+                    <label className={"text-white mr-2"}  htmlFor="">Year</label>
+                    <input className={"w-3/4 py-1 px-2 rounded-sm bg-slate-700 outline-none"}  type="text"/>
+                </div>
+                <div className={"mb-2 flex flex-col "}>
+                    <label className={"text-white "} htmlFor="">Duration</label>
+                    <input className={"w-3/4 py-1 px-2 rounded-sm bg-slate-700 outline-none"}  type="text"/>
+                </div>
+                <div className={"mb-2"}>
+                    <label className={"text-white mr-2"}  htmlFor="type">type</label>
+                    <select className={"py-1 px-2 rounded-sm "} name="type" id="type">
+                        <option value="Movie">Movie</option>
+                        <option value="Tv Shows">Tv shows</option>
+                    </select>
+                </div>
+                <div className={"mb-2"}>
+                    <label className={"text-white mr-2"} htmlFor="type">Genre</label>
+                    <select className={"py-1 px-2 rounded-sm "}  name="type" id="type">
+                        <option value="Action">Action</option>
+                        <option value="Comedy">Comedy</option>
+                        <option value="Fantasize">Fantasize</option>
+                    </select>
+                </div>
+                <div className={"mb-2"}>
+                    <label className={"text-white mr-2"} htmlFor="">Banner image</label>
+                    <input className={"py-1 px-2 rounded-sm "} type="file" id={"Banner image"}/>
+                </div>
+                <div className={"mb-2"}>
+                    <label className={"text-white mr-2"} htmlFor="description">Name image</label>
+                    <input className={"py-1 px-2 rounded-sm "} type="file" id={"Name image "}/>
+                </div>
+                <div className={"mb-2 flex flex-col"}>
+                    <label className={"text-white"} htmlFor="description">Description</label>
+                    <textarea className={"rounded-sm bg-slate-700 outline-none max-[768px]:w-3/4"} name="description" id="description" cols="30" rows="10"></textarea>
+                </div>
 
             </form>
         </main>
+        </>
     );
 };
 

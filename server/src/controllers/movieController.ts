fe2ec35970d000abefa24b7fba70cc9395965354
Oleh <oleggulevic8@gmail.com>
2024-09-10 18:35:04 +0200

    add adding movie implementation

--- server/src/controllers/movieController.ts
+++ server/src/controllers/movieController.ts
@@ -6,13 +6,13 @@ import path from 'path'
 class MovieController{
     async create(req, res){
         const {name, year, duration, description, typeId, genreId, trailer, banner_trailer } = req.body;
-        console.log(req.files)
+        console.log(req.body)
         const {name_image, banner_image} = req.files;
 
         let name_image_filename = uuid.v4()+".jpg";
         let banner_image_filename = uuid.v4()+".jpg";
-        name_image.mv(path.resolve(__dirname, '..','..', 'static', name_image_filename))
-        banner_image.mv(path.resolve(__dirname, '..', '..', 'static', banner_image_filename))
+        name_image.mv(path.resolve(__dirname, '..','..', 'static','name_images', name_image_filename))
+        banner_image.mv(path.resolve(__dirname, '..', '..', 'static', 'banner_images', banner_image_filename))
         console.log(banner_image_filename)
 
         const newMovie = await Movie.create({name:name, year:year, description:description, duration:duration, name_img:name_image_filename, banner_img:banner_image_filename, typeId:typeId, genreId:genreId, trailer:trailer, banner_trailer:banner_trailer});

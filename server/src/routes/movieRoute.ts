a104a4ad97d1a95fae4713143eca1846a0612d69
Oleh <oleggulevic8@gmail.com>
2024-09-28 08:30:16 +0200

    changes in the vercel.json

--- server/src/routes/movieRoute.ts
+++ server/src/routes/movieRoute.ts
@@ -6,7 +6,9 @@ const router = Router();
 router.post('/', MovieController.create);
 router.get('/', MovieController.getAll);
 router.get('/:id', MovieController.getOne);
-
+router.get('/hello',(req,res)=>{
+  res.json("Hello World")
+})
 
 
 export default router;

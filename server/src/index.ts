6f42bbcab087e6c8a07075c3c8b53123515ca55e
Oleh <oleggulevic8@gmail.com>
2024-10-01 17:32:52 +0200

    changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,12 +14,7 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors({
-    origin: 'https://startling-empanada-d42fde.netlify.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-    optionsSuccessStatus: 200
-}));
+app.use(cors());
 
 
 app.use(express.json());
